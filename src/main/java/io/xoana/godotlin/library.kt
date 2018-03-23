33de1fd173ec7253140894ad450096bfa8d20371
Joseph Catrambone <jo.jcat@gmail.com>
2018-03-22 22:43:48 -0700

    First import of source files + build gradle.

---
+++ src/main/java/io/xoana/godotlin/library.kt
@@ -0,0 +1,13 @@
+package io.xoana.godotlin
+
+fun exportMethod() {
+	println("This prints a string to stdout!");
+}
+
+fun returnAnInteger(): Int {
+	return 17;
+}
+
+fun isIntegerBiggerThanTen(x:Int): Boolean {
+	return x > 10
+}
